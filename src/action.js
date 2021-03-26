6ec3ad876abab2f810c20fa25ac23795031c60cc
AlexxNB <iam@alexxnb.ru>
2021-03-26 12:04:24 +0300

    fix: :bug: Remove fullpath from workflow_id

--- src/action.js
+++ src/action.js
@@ -4,7 +4,7 @@ import {checkRepo} from '@tracker';
 (async ()=>{
     try {
         const [owner,repo] = process.env['GITHUB_REPOSITORY'].split('/');
-        const workflow_id = process.env['GITHUB_WORKFLOW'];
+        const workflow_id = process.env['GITHUB_WORKFLOW'].split('/').pop();
         const opts = {
             token: core.getInput('token'),
             srcOwner: core.getInput('owner'),
